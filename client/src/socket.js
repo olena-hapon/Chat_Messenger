ac046717f79a7e1fbc9867661249f8c5b3c78f5b
Olena Hapon <olenagapon90gmail.com>
2025-10-28 16:41:41 +0200

    Added changes

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,6 +3,11 @@ import { io } from "socket.io-client";
 const socket = io("https://chat-messenger-yiim.onrender.com", {
   transports: ["websocket"],
   withCredentials: true,
+  secure: true,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
 });
 
 export default socket;
+
