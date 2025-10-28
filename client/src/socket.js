a96d2fbd322e9f0ca5063f304b397bdf26437ae3
Olena Hapon <olenagapon90gmail.com>
2025-10-28 12:38:12 +0200

    Added changes

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3000", {
-    transports: ["websocket"],
-    withCredentials: true,
+
+const socket = io("https://chat-messenger-yiim.onrender.com", {
+  transports: ["websocket"],
+  withCredentials: true,
 });
+
 export default socket;
